b63b7a5839e8719bc56dddc00200769a7199ec75
benijael <benijael@soopha-network.com>
2026-03-25 23:24:52 +0100

    Restrict user listing to admins only
    Added admins_only decorator to the user listing route to restrict access.

--- CTFd/users.py
+++ CTFd/users.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request, url_for
 from CTFd.models import Users
 from CTFd.utils import config
 from CTFd.utils.decorators import authed_only
+from CTFd.utils.decorators import admins_only
 from CTFd.utils.decorators.visibility import (
     check_account_visibility,
     check_score_visibility,
@@ -14,7 +15,7 @@ users = Blueprint("users", __name__)
 
 
 @users.route("/users")
-@check_account_visibility
+@admins_only  # ← ajoute cette ligne
 def listing():
     q = request.args.get("q")
     field = request.args.get("field", "name")
